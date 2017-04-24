18587b0ea8dcfb41ce29066c4cb224fc7c6a9706
kaviarasukr <kvrs66@gmail.com>
2017-04-24 20:42:18 +0100

    app js

---
+++ app.js
@@ -0,0 +1,23 @@
+(function() {
+
+    var app = angular.module('funwithcountries', []);
+
+    app.controller('CountryController', function() {
+        this.countries = [
+            {
+                name: 'Germany',
+                code: 'de',
+                states: [{ name: 'Bavaria'}, {name: 'Berlin'}]
+            },
+            {
+                name: 'United States',
+                code: 'us',
+                states: [{ name: 'California'}, {name: 'Maryland'}]
+            },
+            {
+                name: 'Luxembourg',
+                code: 'lu'
+            }];
+    });
+
+})();
